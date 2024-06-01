0331cc04df61c2fd7467448dc4f003c444309ba2
Nhân Nguyễn <nthiennhan1611@2qd3w8.onmicrosoft.com>
2024-06-01 12:01:45 +0700

    update

---
+++ Genshin/main.js
@@ -0,0 +1,12 @@
+const nextButton = document.querySelector('.next');
+const prevButton = document.querySelector('.prev');
+
+nextButton.addEventListener('click', () => {
+    const items = document.querySelectorAll('.item');
+    document.querySelector('.slide').appendChild(items[1]);
+});
+
+prevButton.addEventListener('click', () => {
+    const items = document.querySelectorAll('.item');
+    document.querySelector('.slide').prepend(items[items.length - 1]);
+});
